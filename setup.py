d2bbebf808cb236efd8a43df18cc917b017c898f
tank-overlord <76145686+tank-overlord@users.noreply.github.com>
2021-02-04 20:14:53 -0600

    Update

--- setup.py
+++ setup.py
@@ -29,13 +29,12 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
     ],
     install_requires=required,
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     include_package_data=True,
 )
